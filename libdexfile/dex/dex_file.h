949c197585d383755d9f04c024507959b35e722d
Martin Stjernholm <mast@google.com>
2024-02-16 20:33:42 +0000

    Clarify the format and assumptions on the dex file locations.
    libnativeloader now uses them more often to determine how to load
    libraries based on the java caller location, and then it relies on them
    being absolute paths.
    
    Test: n/a - comment change only
    Bug: 237577392
    Change-Id: I6700c0182e3c942a76880307c5947bfca4c1d0d9

--- libdexfile/dex/dex_file.h
+++ libdexfile/dex/dex_file.h
@@ -935,7 +935,17 @@ class DexFile {
   // This is different to the "data section" in the standard dex header.
   ArrayRef<const uint8_t> const data_;
 
-  // Typically the dex file name when available, alternatively some identifying string.
+  // The full absolute path to the dex file, if it was loaded from disk.
+  //
+  // Can also be a path to a multidex container (typically apk), followed by
+  // DexFileLoader.kMultiDexSeparator (i.e. '!') and the file inside the
+  // container.
+  //
+  // On host this may not be an absolute path.
+  //
+  // On device libnativeloader uses this to determine the location of the java
+  // package or shared library, which decides where to load native libraries
+  // from.
   //
   // The ClassLinker will use this to match DexFiles the boot class
   // path to DexCache::GetLocation when loading from an image.
