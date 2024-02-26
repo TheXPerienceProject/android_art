hopez13/art

Implement the new method trace format
Implement the new method trace format. The new method tracing format can
be requested by passing appropriate flags. This CL also adds a test for
this format. More details about the new format are here:
go/method-tracing-format-v2

Bug: 259258187
Test: art/test.py -t 2246

Change-Id: I6dbdab8126703bc5f4d1fb6a6004f69e1cd5fe25

runtime/trace.h
@@ -48,6 +48,8 @@ class DexFile;
 class ShadowFrame;
 class Thread;
 
+struct MethodTraceRecord;
+
 using DexIndexBitSet = std::bitset<65536>;
 
 enum TracingMode {
@@ -146,6 +148,7 @@ class TraceWriter {
               TraceClockSource clock_source,
               size_t buffer_size,
               int num_trace_buffers,
+              int trace_format_version,
               uint32_t clock_overhead_ns);
 
   // This encodes all the events in the per-thread trace buffer and writes it to the trace file /
@@ -210,6 +213,25 @@ class TraceWriter {
   int GetMethodTraceIndex(uintptr_t* current_buffer);
 
  private:
+  void ReadValuesFromRecord(uintptr_t* method_trace_entries,
+                            size_t record_index,
+                            MethodTraceRecord& record,
+                            bool has_thread_cpu_clock,
+                            bool has_wall_clock);
+
+  void FlushEntriesFormatV1(uintptr_t* method_trace_entries,
+                            size_t tid,
+                            const std::unordered_map<ArtMethod*, std::string>& method_infos,
+                            size_t num_records,
+                            size_t* current_index,
+                            uint8_t* init_buffer_ptr) REQUIRES(tracing_lock_);
+
+  void FlushEntriesFormatV0(uintptr_t* method_trace_entries,
+                            size_t tid,
+                            const std::unordered_map<ArtMethod*, std::string>& method_infos,
+                            size_t end_offset,
+                            size_t* current_index,
+                            uint8_t* buffer_ptr) REQUIRES(tracing_lock_);
   // Get a 32-bit id for the method and specify if the method hasn't been seen before. If this is
   // the first time we see this method record information (like method name, declaring class etc.,)
   // about the method.
@@ -236,6 +258,15 @@ class TraceWriter {
                         TraceAction action,
                         uint32_t thread_clock_diff,
                         uint32_t wall_clock_diff) REQUIRES(tracing_lock_);
+
+  // Encodes the header for the events block.. This assumes that there is enough space reserved to
+  // encode the entry.
+  void EncodeEventBlockHeader(uint8_t* ptr,
+                              uint32_t thread_id,
+                              uint32_t method_index,
+                              uint32_t init_thread_clock_time,
+                              uint32_t init_wall_clock_time,
+                              uint16_t num_records) REQUIRES(tracing_lock_);
 
   // Ensures there is sufficient space in the buffer to record the requested_size. If there is not
   // enough sufficient space the current contents of the buffer are written to the file and
@@ -291,11 +322,17 @@ class TraceWriter {
   // Size of buf_.
   const size_t buffer_size_;
 
+  // Version of trace output
+  const int trace_format_version_;
+
   // Time trace was created.
   const uint64_t start_time_;
 
   // Did we overflow the buffer recording traces?
   bool overflow_;
+
+  // Total number of records flushed to file.
+  size_t num_records_;
 
   // Clock overhead.
   const uint32_t clock_overhead_ns_;
@@ -321,6 +358,9 @@ class Trace final : public instrumentation::InstrumentationListener {
     kTraceClockSourceWallClock = 0x010,
     kTraceClockSourceThreadCpu = 0x100,
   };
+
+  static const int kTraceFormatVersionFlagMask = 0b110;
+  static const int kTraceFormatVersionShift = 1;
 
   enum class TraceMode {
     kMethodTracing,

test/2246-trace-v2/dump_trace.cc
@@ -1,0 +1,231 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#include <map>
+#include <memory>
+
+#include "base/leb128.h"
+#include "base/os.h"
+#include "base/unix_file/fd_file.h"
+#include "jni.h"
+
+namespace art {
+namespace {
+
+static const int kMagicValue = 0x574f4c53;
+static const int kVersionDualClock = 0xf5;
+static const int kThreadInfo = 0;
+static const int kMethodInfo = 1;
+static const int kTraceEntries = 2;
+static const int kTraceActionBits = 2;
+static const int kSummary = 3;
+
+int ReadNumber(int num_bytes, uint8_t* header) {
+  int number = 0;
+  for (int i = 0; i < num_bytes; i++) {
+    number += header[i] << (i * 8);
+  }
+  return number;
+}
+
+bool ProcessThreadOrMethodInfo(std::unique_ptr<File>& file, std::map<int, std::string>& name_map) {
+  uint8_t header[6];
+  if (!file->ReadFully(&header, sizeof(header))) {
+    printf("Couldn't read header\n");
+    return false;
+  }
+  int id = ReadNumber(4, header);
+  int length = ReadNumber(2, header + 4);
+
+  char* name = new char[length];
+  if (!file->ReadFully(name, length)) {
+    delete[] name;
+    return false;
+  }
+  std::string str(name, length);
+  std::replace(str.begin(), str.end(), '\t', ' ');
+  name_map.emplace(id, str);
+  delete[] name;
+  return true;
+}
+
+void print_trace_entry(const std::string& thread_name,
+                       const std::string& method_name,
+                       int* curr_depth,
+                       int event_type) {
+  std::string entry;
+  for (int i = 0; i < *curr_depth; i++) {
+    entry.push_back('.');
+  }
+  if (event_type == 0) {
+    *curr_depth += 1;
+    entry.append(".>> ");
+  } else if (event_type == 1) {
+    *curr_depth -= 1;
+    entry.append("<< ");
+  } else if (event_type == 2) {
+    *curr_depth -= 1;
+    entry.append("<<E ");
+  } else {
+    entry.append("?? ");
+  }
+  entry.append(thread_name);
+  entry.append(" ");
+  entry.append(method_name);
+  printf("%s", entry.c_str());
+}
+
+bool ProcessTraceEntries(std::unique_ptr<File>& file,
+                         std::map<int, int>& curr_depth_map,
+                         std::map<int, std::string>& thread_map,
+                         std::map<int, std::string>& method_map,
+                         bool is_dual_clock,
+                         const char* thread_name_filter) {
+  uint8_t header[20];
+  int header_size = is_dual_clock ? 20 : 16;
+  if (!file->ReadFully(header, header_size)) {
+    return false;
+  }
+
+  uint32_t thread_id = ReadNumber(4, header);
+  uint32_t method_value = ReadNumber(4, header + 4);
+  int offset = 8;
+  if (is_dual_clock) {
+    // Read timestamp.
+    ReadNumber(4, header + offset);
+    offset += 4;
+  }
+  // Read timestamp.
+  ReadNumber(4, header + offset);
+  offset += 4;
+  int num_records = ReadNumber(2, header + offset);
+  offset += 2;
+  int total_size = ReadNumber(2, header + offset);
+  uint8_t* buffer = new uint8_t[total_size];
+  if (!file->ReadFully(buffer, total_size)) {
+    delete[] buffer;
+    return false;
+  }
+
+  const uint8_t* curr_buff_ptr = buffer;
+  int32_t method_id = method_value >> kTraceActionBits;
+  uint8_t event_type = method_value & 0x3;
+  int curr_depth = 0;
+  if (curr_depth_map.find(thread_id) != curr_depth_map.end()) {
+    curr_depth = curr_depth_map[thread_id];
+  }
+  std::string thread_name = thread_map[thread_id];
+  bool print_thread_events = (thread_name.compare(thread_name_filter) == 0);
+  if (method_map.find(method_id) == method_map.end()) {
+    printf("No entry for init method %d\n", method_id);
+  }
+  if (print_thread_events) {
+    print_trace_entry(thread_name, method_map[method_id], &curr_depth, event_type);
+  }
+  for (int i = 0; i < num_records; i++) {
+    uint32_t diff = 0;
+    bool success = DecodeUnsignedLeb128Checked(&curr_buff_ptr, buffer + total_size - 1, &diff);
+    if (!success) {
+      printf("Reading past the buffer???\n");
+    }
+    int32_t curr_method_value = method_value + diff;
+    method_id = curr_method_value >> kTraceActionBits;
+    event_type = curr_method_value & 0x3;
+    if (print_thread_events) {
+      print_trace_entry(thread_name, method_map[method_id], &curr_depth, event_type);
+    }
+    // Read timestamps
+    DecodeUnsignedLeb128(&curr_buff_ptr);
+    if (is_dual_clock) {
+      DecodeUnsignedLeb128(&curr_buff_ptr);
+    }
+  }
+  curr_depth_map[thread_id] = curr_depth;
+  return true;
+}
+
+extern "C" JNIEXPORT void JNICALL Java_Main_dumpTrace(JNIEnv* env,
+                                                      jclass,
+                                                      jstring fileName,
+                                                      jstring threadName) {
+  const char* file_name = env->GetStringUTFChars(fileName, nullptr);
+  const char* thread_name = env->GetStringUTFChars(threadName, nullptr);
+  std::map<int, std::string> thread_map;
+  std::map<int, std::string> method_map;
+  std::map<int, int> curr_depth_map;
+
+  std::unique_ptr<File> file(OS::OpenFileForReading(file_name));
+  if (file == nullptr) {
+    printf("Couldn't open file\n");
+    return;
+  }
+
+  uint8_t header[32];
+  const bool success = file->ReadFully(&header, sizeof(header));
+  if (!success) {
+    printf("Couldn't read header\n");
+    return;
+  }
+  int magic_value = ReadNumber(4, header);
+  if (magic_value != kMagicValue) {
+    printf("Incorrect magic value\n");
+    return;
+  }
+  int version = ReadNumber(2, header + 4);
+  if (success) {
+    printf("version=%0x\n", version);
+  }
+
+  bool is_dual_clock = (version == kVersionDualClock);
+  bool has_entries = true;
+  while (has_entries) {
+    uint8_t entry_header;
+    if (!file->ReadFully(&entry_header, sizeof(entry_header))) {
+      break;
+    }
+    switch (entry_header) {
+      case kThreadInfo:
+        if (!ProcessThreadOrMethodInfo(file, thread_map)) {
+          has_entries = false;
+        }
+        break;
+      case kMethodInfo:
+        if (!ProcessThreadOrMethodInfo(file, method_map)) {
+          has_entries = false;
+        }
+        break;
+      case kTraceEntries:
+        ProcessTraceEntries(
+            file, curr_depth_map, thread_map, method_map, is_dual_clock, thread_name);
+        break;
+      case kSummary:
+        has_entries = false;
+        break;
+      default:
+        printf("Invalid Header %d\n", entry_header);
+        has_entries = false;
+        break;
+    }
+  }
+
+  env->ReleaseStringUTFChars(fileName, file_name);
+  env->ReleaseStringUTFChars(threadName, thread_name);
+}
+
+}  // namespace
+}  // namespace art

TheXPerienceProject/android_art

runtime/trace.h
@@ -48,6 +48,8 @@ class DexFile;
 class ShadowFrame;
 class Thread;
 
+struct MethodTraceRecord;
+
 using DexIndexBitSet = std::bitset<65536>;
 
 enum TracingMode {
@@ -146,6 +148,7 @@ class TraceWriter {
               TraceClockSource clock_source,
               size_t buffer_size,
               int num_trace_buffers,
+              int trace_format_version,
               uint32_t clock_overhead_ns);
 
   // This encodes all the events in the per-thread trace buffer and writes it to the trace file /
@@ -210,6 +213,25 @@ class TraceWriter {
   int GetMethodTraceIndex(uintptr_t* current_buffer);
 
  private:
+  void ReadValuesFromRecord(uintptr_t* method_trace_entries,
+                            size_t record_index,
+                            MethodTraceRecord& record,
+                            bool has_thread_cpu_clock,
+                            bool has_wall_clock);
+
+  void FlushEntriesFormatV2(uintptr_t* method_trace_entries,
+                            size_t tid,
+                            const std::unordered_map<ArtMethod*, std::string>& method_infos,
+                            size_t num_records,
+                            size_t* current_index,
+                            uint8_t* init_buffer_ptr) REQUIRES(tracing_lock_);
+
+  void FlushEntriesFormatV1(uintptr_t* method_trace_entries,
+                            size_t tid,
+                            const std::unordered_map<ArtMethod*, std::string>& method_infos,
+                            size_t end_offset,
+                            size_t* current_index,
+                            uint8_t* buffer_ptr) REQUIRES(tracing_lock_);
   // Get a 32-bit id for the method and specify if the method hasn't been seen before. If this is
   // the first time we see this method record information (like method name, declaring class etc.,)
   // about the method.
@@ -236,6 +258,15 @@ class TraceWriter {
                         TraceAction action,
                         uint32_t thread_clock_diff,
                         uint32_t wall_clock_diff) REQUIRES(tracing_lock_);
+
+  // Encodes the header for the events block. This assumes that there is enough space reserved to
+  // encode the entry.
+  void EncodeEventBlockHeader(uint8_t* ptr,
+                              uint32_t thread_id,
+                              uint32_t method_index,
+                              uint32_t init_thread_clock_time,
+                              uint32_t init_wall_clock_time,
+                              uint16_t num_records) REQUIRES(tracing_lock_);
 
   // Ensures there is sufficient space in the buffer to record the requested_size. If there is not
   // enough sufficient space the current contents of the buffer are written to the file and
@@ -291,11 +322,17 @@ class TraceWriter {
   // Size of buf_.
   const size_t buffer_size_;
 
+  // Version of trace output
+  const int trace_format_version_;
+
   // Time trace was created.
   const uint64_t start_time_;
 
   // Did we overflow the buffer recording traces?
   bool overflow_;
+
+  // Total number of records flushed to file.
+  size_t num_records_;
 
   // Clock overhead.
   const uint32_t clock_overhead_ns_;
@@ -321,6 +358,11 @@ class Trace final : public instrumentation::InstrumentationListener {
     kTraceClockSourceWallClock = 0x010,
     kTraceClockSourceThreadCpu = 0x100,
   };
+
+  static const int kFormatV1 = 0;
+  static const int kFormatV2 = 1;
+  static const int kTraceFormatVersionFlagMask = 0b110;
+  static const int kTraceFormatVersionShift = 1;
 
   enum class TraceMode {
     kMethodTracing,

test/2246-trace-v2/dump_trace.cc
@@ -1,0 +1,233 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#include <map>
+#include <memory>
+
+#include "base/leb128.h"
+#include "base/os.h"
+#include "base/unix_file/fd_file.h"
+#include "jni.h"
+
+namespace art {
+namespace {
+
+static const int kMagicValue = 0x574f4c53;
+static const int kVersionDualClock = 0xf5;
+static const int kThreadInfo = 0;
+static const int kMethodInfo = 1;
+static const int kTraceEntries = 2;
+static const int kTraceActionBits = 2;
+static const int kSummary = 3;
+
+int ReadNumber(int num_bytes, uint8_t* header) {
+  int number = 0;
+  for (int i = 0; i < num_bytes; i++) {
+    number += header[i] << (i * 8);
+  }
+  return number;
+}
+
+bool ProcessThreadOrMethodInfo(std::unique_ptr<File>& file, std::map<int, std::string>& name_map) {
+  uint8_t header[6];
+  if (!file->ReadFully(&header, sizeof(header))) {
+    printf("Couldn't read header\n");
+    return false;
+  }
+  int id = ReadNumber(4, header);
+  int length = ReadNumber(2, header + 4);
+
+  char* name = new char[length];
+  if (!file->ReadFully(name, length)) {
+    delete[] name;
+    return false;
+  }
+  std::string str(name, length);
+  std::replace(str.begin(), str.end(), '\t', ' ');
+  name_map.emplace(id, str);
+  delete[] name;
+  return true;
+}
+
+void print_trace_entry(const std::string& thread_name,
+                       const std::string& method_name,
+                       int* current_depth,
+                       int event_type) {
+  std::string entry;
+  for (int i = 0; i < *current_depth; i++) {
+    entry.push_back('.');
+  }
+  if (event_type == 0) {
+    *current_depth += 1;
+    entry.append(".>> ");
+  } else if (event_type == 1) {
+    *current_depth -= 1;
+    entry.append("<< ");
+  } else if (event_type == 2) {
+    *current_depth -= 1;
+    entry.append("<<E ");
+  } else {
+    entry.append("?? ");
+  }
+  entry.append(thread_name);
+  entry.append(" ");
+  entry.append(method_name);
+  printf("%s", entry.c_str());
+}
+
+bool ProcessTraceEntries(std::unique_ptr<File>& file,
+                         std::map<int, int>& current_depth_map,
+                         std::map<int, std::string>& thread_map,
+                         std::map<int, std::string>& method_map,
+                         bool is_dual_clock,
+                         const char* thread_name_filter) {
+  uint8_t header[20];
+  int header_size = is_dual_clock ? 20 : 16;
+  if (!file->ReadFully(header, header_size)) {
+    return false;
+  }
+
+  uint32_t thread_id = ReadNumber(4, header);
+  uint32_t method_value = ReadNumber(4, header + 4);
+  int offset = 8;
+  if (is_dual_clock) {
+    // Read timestamp.
+    ReadNumber(4, header + offset);
+    offset += 4;
+  }
+  // Read timestamp.
+  ReadNumber(4, header + offset);
+  offset += 4;
+  int num_records = ReadNumber(2, header + offset);
+  offset += 2;
+  int total_size = ReadNumber(2, header + offset);
+  uint8_t* buffer = new uint8_t[total_size];
+  if (!file->ReadFully(buffer, total_size)) {
+    delete[] buffer;
+    return false;
+  }
+
+  const uint8_t* current_buffer_ptr = buffer;
+  int32_t method_id = method_value >> kTraceActionBits;
+  uint8_t event_type = method_value & 0x3;
+  int current_depth = 0;
+  if (current_depth_map.find(thread_id) != current_depth_map.end()) {
+    // Get the current call stack depth. If it is the first method we are seeing on this thread
+    // then this map wouldn't haven an entry we start with the depth of 0.
+    current_depth = current_depth_map[thread_id];
+  }
+  std::string thread_name = thread_map[thread_id];
+  bool print_thread_events = (thread_name.compare(thread_name_filter) == 0);
+  if (method_map.find(method_id) == method_map.end()) {
+    LOG(FATAL) << "No entry for init method " << method_id;
+  }
+  if (print_thread_events) {
+    print_trace_entry(thread_name, method_map[method_id], &current_depth, event_type);
+  }
+  for (int i = 0; i < num_records; i++) {
+    int32_t diff = 0;
+    bool success = DecodeSignedLeb128Checked(&current_buffer_ptr, buffer + total_size - 1, &diff);
+    if (!success) {
+      LOG(FATAL) << "Reading past the buffer???";
+    }
+    int32_t curr_method_value = method_value + diff;
+    method_id = curr_method_value >> kTraceActionBits;
+    event_type = curr_method_value & 0x3;
+    if (print_thread_events) {
+      print_trace_entry(thread_name, method_map[method_id], &current_depth, event_type);
+    }
+    // Read timestamps
+    DecodeUnsignedLeb128(&current_buffer_ptr);
+    if (is_dual_clock) {
+      DecodeUnsignedLeb128(&current_buffer_ptr);
+    }
+  }
+  current_depth_map[thread_id] = current_depth;
+  return true;
+}
+
+extern "C" JNIEXPORT void JNICALL Java_Main_dumpTrace(JNIEnv* env,
+                                                      jclass,
+                                                      jstring fileName,
+                                                      jstring threadName) {
+  const char* file_name = env->GetStringUTFChars(fileName, nullptr);
+  const char* thread_name = env->GetStringUTFChars(threadName, nullptr);
+  std::map<int, std::string> thread_map;
+  std::map<int, std::string> method_map;
+  std::map<int, int> current_depth_map;
+
+  std::unique_ptr<File> file(OS::OpenFileForReading(file_name));
+  if (file == nullptr) {
+    printf("Couldn't open file\n");
+    return;
+  }
+
+  uint8_t header[32];
+  const bool success = file->ReadFully(&header, sizeof(header));
+  if (!success) {
+    printf("Couldn't read header\n");
+    return;
+  }
+  int magic_value = ReadNumber(4, header);
+  if (magic_value != kMagicValue) {
+    printf("Incorrect magic value\n");
+    return;
+  }
+  int version = ReadNumber(2, header + 4);
+  if (success) {
+    printf("version=%0x\n", version);
+  }
+
+  bool is_dual_clock = (version == kVersionDualClock);
+  bool has_entries = true;
+  while (has_entries) {
+    uint8_t entry_header;
+    if (!file->ReadFully(&entry_header, sizeof(entry_header))) {
+      break;
+    }
+    switch (entry_header) {
+      case kThreadInfo:
+        if (!ProcessThreadOrMethodInfo(file, thread_map)) {
+          has_entries = false;
+        }
+        break;
+      case kMethodInfo:
+        if (!ProcessThreadOrMethodInfo(file, method_map)) {
+          has_entries = false;
+        }
+        break;
+      case kTraceEntries:
+        ProcessTraceEntries(
+            file, current_depth_map, thread_map, method_map, is_dual_clock, thread_name);
+        break;
+      case kSummary:
+        has_entries = false;
+        break;
+      default:
+        printf("Invalid Header %d\n", entry_header);
+        has_entries = false;
+        break;
+    }
+  }
+
+  env->ReleaseStringUTFChars(fileName, file_name);
+  env->ReleaseStringUTFChars(threadName, thread_name);
+}
+
+}  // namespace
+}  // namespace art