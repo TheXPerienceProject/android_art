hopez13/art

Update odrefresh for Pre-reboot Dexopt.
- Add a mode to only generate boot image. The boot image will be used
  for dexopting apps during Pre-reboot Dexopt.
- Make odrefresh headers accessible by other components in the ART
  module. The headers contain odrefresh exit codes that artd needs to
  check against.

Bug: 311377497
Test: atest art_standalone_odrefresh_tests
Change-Id: I56ba79646944b29b0c016a55a5f57a5a7c71d1a6

odrefresh/odrefresh.cc
@@ -2150,7 +2150,8 @@ WARN_UNUSED ExitCode OnDeviceRefresh::Compile(OdrMetrics& metrics,
   }
 
   // Don't compile system server if the compilation of BCP failed.
-  if (!system_server_isa_failed && !compilation_options.system_server_jars_to_compile.empty()) {
+  if (!system_server_isa_failed && !compilation_options.system_server_jars_to_compile.empty() &&
+      !config_.GetOnlyBootImage()) {
     OdrMetrics::Stage stage = OdrMetrics::Stage::kSystemServerClasspath;
     CompilationResult ss_result = CompileSystemServer(
         staging_dir, compilation_options.system_server_jars_to_compile, advance_animation_progress);

odrefresh/odrefresh_main.cc
@@ -164,6 +164,8 @@ int InitializeConfig(int argc, char** argv, OdrConfig* config) {
       config->SetRefresh(false);
     } else if (ArgumentEquals(arg, "--minimal")) {
       config->SetMinimal(true);
+    } else if (ArgumentEquals(arg, "--only-boot-image")) {
+      config->SetOnlyBootImage(true);
     } else {
       ArgumentError("Unrecognized argument: '%s'", arg);
     }
@@ -248,6 +250,7 @@ NO_RETURN void UsageHelp(const char* argv0) {
   UsageMsg("                                 Compiler filter that overrides");
   UsageMsg("                                 dalvik.vm.systemservercompilerfilter");
   UsageMsg("--minimal                        Generate a minimal boot image only.");
+  UsageMsg("--only-boot-image                Generate boot images only.");
 
   exit(EX_USAGE);
 }

odrefresh/odrefresh_test.cc
@@ -993,5 +993,22 @@ TEST_F(OdRefreshTest, CompileSystemServerChoosesBootImage_OnSystem) {
       ExitCode::kCompilationSuccess);
 }
 
+TEST_F(OdRefreshTest, OnlyBootImage) {
+  config_.SetOnlyBootImage(true);
+
+  // Primary.
+  EXPECT_CALL(*mock_exec_utils_, DoExecAndReturnCode(Contains(Flag("--dex-file=", core_oj_jar_))))
+      .Times(2)
+      .WillRepeatedly(Return(0));
+
+  // Mainline extension.
+  EXPECT_CALL(*mock_exec_utils_, DoExecAndReturnCode(Contains(Flag("--dex-file=", conscrypt_jar_))))
+      .Times(2)
+      .WillRepeatedly(Return(0));
+
+  EXPECT_EQ(odrefresh_->Compile(*metrics_, CompilationOptions::CompileAll(*odrefresh_)),
+            ExitCode::kCompilationSuccess);
+}
+
 }  // namespace odrefresh
 }  // namespace art

TheXPerienceProject/android_art

odrefresh/odrefresh.cc
@@ -2178,7 +2178,8 @@ WARN_UNUSED ExitCode OnDeviceRefresh::Compile(OdrMetrics& metrics,
   }
 
   // Don't compile system server if the compilation of BCP failed.
-  if (!system_server_isa_failed && !compilation_options.system_server_jars_to_compile.empty()) {
+  if (!system_server_isa_failed && !compilation_options.system_server_jars_to_compile.empty() &&
+      !config_.GetOnlyBootImages()) {
     OdrMetrics::Stage stage = OdrMetrics::Stage::kSystemServerClasspath;
     CompilationResult ss_result = CompileSystemServer(
         staging_dir, compilation_options.system_server_jars_to_compile, advance_animation_progress);

odrefresh/odrefresh_main.cc
@@ -165,6 +165,8 @@ int InitializeConfig(int argc, char** argv, OdrConfig* config) {
       config->SetRefresh(false);
     } else if (ArgumentEquals(arg, "--minimal")) {
       config->SetMinimal(true);
+    } else if (ArgumentEquals(arg, "--only-boot-images")) {
+      config->SetOnlyBootImages(true);
     } else {
       ArgumentError("Unrecognized argument: '%s'", arg);
     }
@@ -247,6 +249,7 @@ NO_RETURN void UsageHelp(const char* argv0) {
   UsageMsg("                                 Compiler filter that overrides");
   UsageMsg("                                 dalvik.vm.systemservercompilerfilter");
   UsageMsg("--minimal                        Generate a minimal boot image only.");
+  UsageMsg("--only-boot-images               Generate boot images only.");
 
   exit(EX_USAGE);
 }

odrefresh/odrefresh_test.cc
@@ -994,5 +994,22 @@ TEST_F(OdRefreshTest, CompileSystemServerChoosesBootImage_OnSystem) {
       ExitCode::kCompilationSuccess);
 }
 
+TEST_F(OdRefreshTest, OnlyBootImages) {
+  config_.SetOnlyBootImages(true);
+
+  // Primary.
+  EXPECT_CALL(*mock_exec_utils_, DoExecAndReturnCode(Contains(Flag("--dex-file=", core_oj_jar_))))
+      .Times(2)
+      .WillRepeatedly(Return(0));
+
+  // Mainline extension.
+  EXPECT_CALL(*mock_exec_utils_, DoExecAndReturnCode(Contains(Flag("--dex-file=", conscrypt_jar_))))
+      .Times(2)
+      .WillRepeatedly(Return(0));
+
+  EXPECT_EQ(odrefresh_->Compile(*metrics_, CompilationOptions::CompileAll(*odrefresh_)),
+            ExitCode::kCompilationSuccess);
+}
+
 }  // namespace odrefresh
 }  // namespace art