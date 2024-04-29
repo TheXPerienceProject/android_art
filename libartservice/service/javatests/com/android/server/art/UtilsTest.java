06ab2453491bdc8086c997b9681f557bb4f8952f
Jiakai Zhang <jiakaiz@google.com>
2024-04-29 14:51:48 +0100

    Support apps with unsupported ISA.
    On a device with no native bridge support, an app with a native lib in a
    foreign ABI can be installed and launched in the device's preferred ABI,
    as long as it doesn't load the native lib. Therefore, we should treat it
    as if it has no native lib.
    
    Bug: 325692552
    Test: atest ArtServiceTests
    Change-Id: I6474607adc6495d8c8918dcd808c3dbf7c43afd5

--- libartservice/service/javatests/com/android/server/art/UtilsTest.java
+++ libartservice/service/javatests/com/android/server/art/UtilsTest.java
@@ -151,7 +151,7 @@ public class UtilsTest {
         Utils.getAllAbis(pkgState);
     }
 
-    @Test(expected = IllegalStateException.class)
+    @Test
     public void testGetAllAbisUnsupportedTranslation() {
         lenient().when(SystemProperties.get(eq("ro.dalvik.vm.isa.x86_64"))).thenReturn("");
 
@@ -159,7 +159,9 @@ public class UtilsTest {
         when(pkgState.getPrimaryCpuAbi()).thenReturn("x86_64");
         when(pkgState.getSecondaryCpuAbi()).thenReturn(null);
 
-        Utils.getAllAbis(pkgState);
+        when(Constants.getPreferredAbi()).thenReturn("armeabi-v7a");
+        assertThat(Utils.getAllAbis(pkgState))
+                .containsExactly(Utils.Abi.create("armeabi-v7a", "arm", true /* isPrimaryAbi */));
     }
 
     @Test
