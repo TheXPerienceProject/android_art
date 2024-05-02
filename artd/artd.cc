hopez13/art

Implement commitPreRebootStagedFiles.
Bug: 311377497
Test: m test-art-host-gtest-art_artd_tests
Test: atest ArtServiceTests
Change-Id: Ia8900baeaa8ed4208f0ecacf454a97a72112ba95

artd/artd.cc
@@ -132,6 +132,7 @@ using ::art::tools::NonFatal;
 using ::ndk::ScopedAStatus;
 
 using TmpProfilePath = ProfilePath::TmpProfilePath;
+using WritableProfilePath = ProfilePath::WritableProfilePath;
 
 constexpr const char* kServiceName = "artd";
 constexpr const char* kPreRebootServiceName = "artd_pre_reboot";
@@ -1346,6 +1347,70 @@ ScopedAStatus Artd::getProfileSize(const ProfilePath& in_profile, int64_t* _aidl
   RETURN_FATAL_IF_PRE_REBOOT(options_);
   std::string profile_path = OR_RETURN_FATAL(BuildProfileOrDmPath(in_profile));
   *_aidl_return = GetSize(profile_path).value_or(0);
+  return ScopedAStatus::ok();
+}
+
+ScopedAStatus Artd::commitPreRebootStagedFiles(
+    const std::vector<ArtifactsPath>& in_artifacts,
+    const std::vector<WritableProfilePath>& in_profiles) {
+  RETURN_FATAL_IF_PRE_REBOOT(options_);
+
+  // Only used as the storage of the string_view's.
+  std::vector<std::unique_ptr<RawArtifactsPath>> artifacts_paths;
+  std::vector<std::unique_ptr<std::string>> profile_paths;
+
+  std::vector<std::pair<std::string_view, std::string_view>> files_to_move;
+  std::vector<std::string_view> files_to_remove;
+
+  for (const ArtifactsPath& artifacts : in_artifacts) {
+    RETURN_FATAL_IF_PRE_REBOOT_MISMATCH(options_, artifacts.isPreReboot, "artifacts");
+
+    ArtifactsPath pre_reboot_artifacts = artifacts;
+    pre_reboot_artifacts.isPreReboot = true;
+
+    auto src_artifacts = std::make_unique<RawArtifactsPath>(
+        OR_RETURN_FATAL(BuildArtifactsPath(pre_reboot_artifacts)));
+    auto dst_artifacts =
+        std::make_unique<RawArtifactsPath>(OR_RETURN_FATAL(BuildArtifactsPath(artifacts)));
+
+    if (OS::FileExists(src_artifacts->oat_path.c_str())) {
+      files_to_move.emplace_back(src_artifacts->oat_path, dst_artifacts->oat_path);
+      files_to_move.emplace_back(src_artifacts->vdex_path, dst_artifacts->vdex_path);
+      if (OS::FileExists(src_artifacts->art_path.c_str())) {
+        files_to_move.emplace_back(src_artifacts->art_path, dst_artifacts->art_path);
+      } else {
+        files_to_remove.push_back(dst_artifacts->art_path);
+      }
+
+      artifacts_paths.push_back(std::move(src_artifacts));
+      artifacts_paths.push_back(std::move(dst_artifacts));
+    }
+  }
+  for (const WritableProfilePath& profile : in_profiles) {
+    RETURN_FATAL_IF_PRE_REBOOT_MISMATCH(options_, PreRebootFlag(profile), "profiles");
+
+    WritableProfilePath pre_reboot_profile = profile;
+    PreRebootFlag(pre_reboot_profile) = true;
+
+    auto src_profile = std::make_unique<std::string>(
+        OR_RETURN_FATAL(BuildWritableProfilePath(pre_reboot_profile)));
+    auto dst_profile =
+        std::make_unique<std::string>(OR_RETURN_FATAL(BuildWritableProfilePath(profile)));
+
+    if (OS::FileExists(src_profile->c_str())) {
+      files_to_move.emplace_back(*src_profile, *dst_profile);
+
+      profile_paths.push_back(std::move(src_profile));
+      profile_paths.push_back(std::move(dst_profile));
+    }
+  }
+
+  OR_RETURN_NON_FATAL(MoveAllOrAbandon(files_to_move, files_to_remove));
+
+  for (const auto& [src_path, dst_path] : files_to_move) {
+    LOG(INFO) << ART_FORMAT("Committed Pre-reboot staged file '{}' to '{}'", src_path, dst_path);
+  }
+
   return ScopedAStatus::ok();
 }
 

TheXPerienceProject/android_art

artd/artd.cc
@@ -129,6 +129,7 @@ using ::art::tools::NonFatal;
 using ::ndk::ScopedAStatus;
 
 using TmpProfilePath = ProfilePath::TmpProfilePath;
+using WritableProfilePath = ProfilePath::WritableProfilePath;
 
 constexpr const char* kServiceName = "artd";
 constexpr const char* kPreRebootServiceName = "artd_pre_reboot";
@@ -1367,6 +1368,61 @@ ScopedAStatus Artd::getProfileSize(const ProfilePath& in_profile, int64_t* _aidl
   RETURN_FATAL_IF_ARG_IS_PRE_REBOOT(in_profile, "profile");
   std::string profile_path = OR_RETURN_FATAL(BuildProfileOrDmPath(in_profile));
   *_aidl_return = GetSize(profile_path).value_or(0);
+  return ScopedAStatus::ok();
+}
+
+ScopedAStatus Artd::commitPreRebootStagedFiles(
+    const std::vector<ArtifactsPath>& in_artifacts,
+    const std::vector<WritableProfilePath>& in_profiles) {
+  RETURN_FATAL_IF_PRE_REBOOT(options_);
+
+  std::vector<std::pair<std::string, std::string>> files_to_move;
+  std::vector<std::string> files_to_remove;
+
+  for (const ArtifactsPath& artifacts : in_artifacts) {
+    RETURN_FATAL_IF_ARG_IS_PRE_REBOOT(artifacts, "artifacts");
+
+    ArtifactsPath pre_reboot_artifacts = artifacts;
+    pre_reboot_artifacts.isPreReboot = true;
+
+    auto src_artifacts = std::make_unique<RawArtifactsPath>(
+        OR_RETURN_FATAL(BuildArtifactsPath(pre_reboot_artifacts)));
+    auto dst_artifacts =
+        std::make_unique<RawArtifactsPath>(OR_RETURN_FATAL(BuildArtifactsPath(artifacts)));
+
+    if (OS::FileExists(src_artifacts->oat_path.c_str())) {
+      files_to_move.emplace_back(src_artifacts->oat_path, dst_artifacts->oat_path);
+      files_to_move.emplace_back(src_artifacts->vdex_path, dst_artifacts->vdex_path);
+      if (OS::FileExists(src_artifacts->art_path.c_str())) {
+        files_to_move.emplace_back(src_artifacts->art_path, dst_artifacts->art_path);
+      } else {
+        files_to_remove.push_back(dst_artifacts->art_path);
+      }
+    }
+  }
+
+  for (const WritableProfilePath& profile : in_profiles) {
+    RETURN_FATAL_IF_ARG_IS_PRE_REBOOT(profile, "profiles");
+
+    WritableProfilePath pre_reboot_profile = profile;
+    PreRebootFlag(pre_reboot_profile) = true;
+
+    auto src_profile = std::make_unique<std::string>(
+        OR_RETURN_FATAL(BuildWritableProfilePath(pre_reboot_profile)));
+    auto dst_profile =
+        std::make_unique<std::string>(OR_RETURN_FATAL(BuildWritableProfilePath(profile)));
+
+    if (OS::FileExists(src_profile->c_str())) {
+      files_to_move.emplace_back(*src_profile, *dst_profile);
+    }
+  }
+
+  OR_RETURN_NON_FATAL(MoveAllOrAbandon(files_to_move, files_to_remove));
+
+  for (const auto& [src_path, dst_path] : files_to_move) {
+    LOG(INFO) << ART_FORMAT("Committed Pre-reboot staged file '{}' to '{}'", src_path, dst_path);
+  }
+
   return ScopedAStatus::ok();
 }
 